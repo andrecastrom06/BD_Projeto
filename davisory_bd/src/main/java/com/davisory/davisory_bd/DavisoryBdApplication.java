885be5051d7b0a90f2b73e1fe99a402aef5c4a0b
andrecastrom06 <alcms@cesar.school>
2025-03-31 13:55:19 -0300

    subindo spring

---
+++ davisory_bd/src/main/java/com/davisory/davisory_bd/DavisoryBdApplication.java
@@ -0,0 +1,13 @@
+package com.davisory.davisory_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavisoryBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavisoryBdApplication.class, args);
+	}
+
+}
