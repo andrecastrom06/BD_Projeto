6ba955f20ff6badc52e5cbdfafde8672e119858d
andrecastrom06 <alcms@cesar.school>
2025-04-05 11:39:32 -0300

    conectei dbeaver e spring, testei conexao e criei classe controller de teste

---
+++ davisory_bd/src/main/java/com/davisory/davisory_bd/controller/TesteController.java
@@ -0,0 +1,13 @@
+package com.davisory.davisory_bd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/inicial")
+    public String ping() {
+        return "PAGINA INICIAL BD DAVISORY";
+    }
+}
