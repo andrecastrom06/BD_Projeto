982a6e37ad34051f9f87219ed8ac60a034db4f75
andrecastrom06 <alcms@cesar.school>
2025-04-09 16:24:50 -0300

    editar produto

--- davisory_bd/src/main/java/com/davisory/davisory_bd/controller/ProdutoController.java
+++ davisory_bd/src/main/java/com/davisory/davisory_bd/controller/ProdutoController.java
@@ -2,10 +2,9 @@ package com.davisory.davisory_bd.controller;
 
 import com.davisory.davisory_bd.dao.ProdutoRepositorio;
 import com.davisory.davisory_bd.model.Produto;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +14,37 @@ public class ProdutoController {
     ProdutoRepositorio produtoRepositorio = new ProdutoRepositorio();
 
     @GetMapping("/produtos")
-    public String exibirPaginaProdutos(Model model) {
+    public String listarProdutos(Model model) {
         List<Produto> produtos = produtoRepositorio.listarProdutos();
         model.addAttribute("produtos", produtos);
         return "produtos";
+    }
+
+    @GetMapping("/produtos/editar/{id}")
+    public String exibirFormularioEdicao(@PathVariable("id") int id, Model model) {
+        Produto produto = produtoRepositorio.buscarPorId(id);
+        if (produto != null) {
+            model.addAttribute("produto", produto);
+            return "editarProduto";
+        } else {
+            return "redirect:/produtos";
+        }
+    }
+
+    @PostMapping("/produtos/atualizar")
+    public String atualizarProduto(
+            @RequestParam("id") int id,
+            @RequestParam("nome") String nome,
+            @RequestParam("descricao") String descricao,
+            @RequestParam("preco") double preco
+    ) {
+        Produto produto = new Produto();
+        produto.setIdProduto(id);
+        produto.setNomeProduto(nome);
+        produto.setDescricaoProduto(descricao);
+        produto.setPrecoProduto(preco);
+
+        produtoRepositorio.atualizarProduto(produto);
+        return "redirect:/produtos";
     }
 }
