b27b7d5faa43f4f2f2f854553d7bcb5f8a614ac2
Clb7cc <clb@cesar.school>
2025-04-29 11:15:22 -0300

    login sem barra

--- davisory_bd/src/main/java/com/davisory/davisory_bd/controller/LoginController.java
+++ davisory_bd/src/main/java/com/davisory/davisory_bd/controller/LoginController.java
@@ -3,14 +3,17 @@ package com.davisory.davisory_bd.controller;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.ui.Model;
+
 import com.davisory.davisory_bd.dao.FuncionarioRepositorio;
 import com.davisory.davisory_bd.dto.LoginDTO;
 import com.davisory.davisory_bd.model.Funcionario;
+
 import jakarta.servlet.http.HttpSession;
 
 
@@ -25,13 +28,13 @@ public class LoginController {
     @PostMapping("/login")
     public String login(@ModelAttribute LoginDTO loginDTO, HttpSession session, Model model) {
         String nome = loginDTO.getNome();
-        String dataStr = loginDTO.getData_contratacao().replaceAll("/", "");
+        String dataStr = loginDTO.getData_contratacao();
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy");
             LocalDate dataContratacao = LocalDate.parse(dataStr, formatter);
             Funcionario funcionario = FuncionarioRepositorio.acharAdministrativoPorNomeEData(nome, dataContratacao);
             if (funcionario != null) {
-                session.setAttribute("usuarioLogado", funcionario); // salvar funcionario logado na sessão
+                session.setAttribute("usuarioLogado", funcionario);
                 return "inicio";
             } else {
                 model.addAttribute("erro", "Login ou senha incorretos.");
